847328ed46baee76c30841b1758af49395b173f9
Savannah <skruse101@gmail.com>
2023-04-06 16:20:42 -0700

    arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+simple function
+ function divide(a,b){
+     return a/b;
+ }
+*/
+
+
+const divide = function(a,b){
+    return a/b;
+}
+
+
+let x = 2;
+const square = x => x ** 2;
+
+
+// arrow function 
+const add = (a,b) => a + b;
